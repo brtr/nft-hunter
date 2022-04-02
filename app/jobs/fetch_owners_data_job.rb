548c3b889559e206d93bd49f40ae40c9e514ed81
Hazel <xp0729@gmail.com>
2022-04-02 10:15:03 +0800

    update fetch owner job

--- app/jobs/fetch_owners_data_job.rb
+++ app/jobs/fetch_owners_data_job.rb
@@ -2,7 +2,7 @@ class FetchOwnersDataJob < ApplicationJob
   queue_as :daily_job
 
   def perform
-    NftOwnerService.fetch_owners
     NftOwnerService.fetch_target_nft_owners_data(1)
+    NftOwnerService.fetch_owners
   end
 end
