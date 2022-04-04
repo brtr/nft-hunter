99971974fc3ef38e7cf7f6eb866aa650980d5477
Hazel <xp0729@gmail.com>
2022-04-04 10:52:47 +0800

    update nft data api

--- app/controllers/api/v1/nfts.rb
+++ app/controllers/api/v1/nfts.rb
@@ -2,10 +2,15 @@ module API
   module V1
     class Nfts < Grape::API
       resource :nfts do
-        desc 'Get target NFT price data'
-        get :price_data do
+        desc 'Get target NFT price data and owners data'
+        get :data do
           nft = Nft.where(slug: params[:slug]).take
-          data = NftHistory::PriceChartService.new(start_date: 7.days.ago.to_date, nft_id: nft.id).get_price_data rescue {}
+          price_data = NftHistory::PriceChartService.new(start_date: 7.days.ago.to_date, nft_id: nft.id).get_price_data rescue {}
+          owners_data = NftOwnerService.get_target_owners_ratio(nft.id) rescue []
+          data = {
+            price_data: price_data,
+            owners_data: owners_data
+          }
 
           present :result, true
           present :data, data, with: Grape::Presenters::Presenter
