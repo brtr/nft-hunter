dcaf972c85f585234179fadba3144b4e71581193
Hazel <xp0729@gmail.com>
2022-05-10 10:27:34 +0800

    remove server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
 server "34.235.134.215", user: "deploy", roles: %w(web)
 server "3.234.236.191", user: "deploy", roles: %w(job)
 server "3.230.144.56", user: "deploy", roles: %w(job)
-server "44.192.108.73", user: "deploy", roles: %w(job)
