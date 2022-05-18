29b47428c5a11d5787d9e4291f7223f6eb1a0033
Hazel <xp0729@gmail.com>
2022-05-18 14:06:35 +0800

    update job server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server "34.235.134.215", user: "deploy", roles: %w(web)
 server "3.234.236.191", user: "deploy", roles: %w(job)
-server "3.230.144.56", user: "deploy", roles: %w(job)
+server "35.175.106.70", user: "deploy", roles: %w(job)
